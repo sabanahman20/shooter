6baf2c5d4af26f033f78c08d08ed6b5c1ce95399
Evgenia Alievsky <daniel.alievsky@gmail.com>
2020-11-08 17:36:15 +0200

    начало работы с канвасом

--- js/shooter.js
+++ js/shooter.js
@@ -1,5 +1,7 @@
-window.addEventListener("load", function()
- {
-	
+window.addEventListener("load", function(){
+	var canvas = document.querySelector("#holst");
+	var scena = canvas.getContext("2d");
+	scena.fillStyle = "purple"
+	scena.fillRect(250,250,100,100);
 });
 
