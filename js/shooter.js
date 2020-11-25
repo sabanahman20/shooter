260c6ec3ef95dc5a8391f8c1267f03e63ef59880
Evgenia Alievsky <daniel.alievsky@gmail.com>
2020-11-25 17:18:55 +0200

    finish demo

--- js/shooter.js
+++ js/shooter.js
@@ -2,8 +2,10 @@ window.addEventListener("load", function(){
 	var canvas = document.querySelector("#holst");
 	var scena = canvas.getContext("2d");
 	var fon = new Image();
+	var meteorSprite = new Image();
 	var meteorits = [];
 	var speedGeneration = 100;
+	var pause = true;
 	function rnd(min, max){//генерато случайного числа от мин до макс.
 		var r = min + Math.random() * (max - min + 1);
 		return Math.floor(r);
@@ -17,7 +19,7 @@ window.addEventListener("load", function(){
 		bullets:[],
 		IsShooting:false,
 		ShootInterval:40,
-		sound:new Audio("sounds/blaster.mp3"),
+		sounds:[new Audio("sounds/blaster.mp3"),new Audio("sounds/bum.mp3")],
 		Move:function(dx){
 			this.X += dx;
 			if(this.X < 0 || this.X > canvas.width - this.Size){
@@ -37,7 +39,7 @@ window.addEventListener("load", function(){
 		Shoot:function(){
 			if(this.IsShooting == false){
 				this.bullets.push(new Bullet());
-				this.sound.play();
+				this.sounds[0].play();
 				this.IsShooting = true;
 			};
 		}
@@ -56,14 +58,25 @@ window.addEventListener("load", function(){
 				if(this.Y < 0){
 					this.Del = true;
 				}
+				else{
+					for(var i = 0; i < meteorits.length; i++){
+						if((this.X >= meteorits[i].X)&&
+						(this.X < meteorits[i].X + meteorits[i].Size)&&
+						(this.Y >= meteorits[i].Y)&&
+						(this.Y < meteorits[i].Y + meteorits[i].Size)){
+							meteorits[i].Del = true;
+							this.Del = true;
+						}
+					}
+				}
 			}
 	}
 	class Meteor{
 		constructor(){
-			this.X = rnd(0,(canvas.width/50)*50); /*rnd(0,600)33*/;
+			this.X = rnd(0,(canvas.width/50 -1 ))*50; /*rnd(0,600)33*/;
 			this.Y = 0;
 			this.Size = 50;
-			this.Speed = 3;
+			this.Speed = rnd(1,5);
 			this.Del = false;
 		}
 		Move(){
@@ -106,32 +119,40 @@ window.addEventListener("load", function(){
 		scena.drawImage(fon,0,0,canvas.width,canvas.height);//очистка всего игрового поля//
 		scena.drawImage(spaceship.sprite,spaceship.X,spaceship.Y,spaceship.Size,spaceship.Size);
 		for(var i = 0; i < meteorits.length; i++){
-			scena.fillRect(meteorits[i].X, meteorits[i].Y,meteorits[i].Size,meteorits[i].Size);
+			scena.drawImage(meteorSprite, meteorits[i].X, meteorits[i].Y,meteorits[i].Size,meteorits[i].Size);
 		}
+		scena.fillStyle = "red";
 		for(var i = 0; i < spaceship.bullets.length; i++){
 			scena.fillRect(spaceship.bullets[i].X, spaceship.bullets[i].Y,spaceship.bullets[i].Size,spaceship.bullets[i].Size);
 		}
 	};
-	
 	function game(){
-		update();
+		if(pause == false){
+			update();
+		}
 		draw();
 		window.requestAnimationFrame(function(){
 			game();
 		});
 	};
+	meteorSprite.src = "images/meteor.png";
 	spaceship.sprite.src = "images/spaceship.png";
 	fon.src = "images/space.jpg";
-	fon.addEventListener("load",function(){
+	fon.addEventListener("load",function(){//alert(play.keyCode)
 		window.addEventListener("keydown",function(play){
-			if(play.keyCode == 39){
-				spaceship.Move(10);
+			if(!pause){
+				if(play.keyCode == 39){
+					spaceship.Move(20);
+				}
+				else if(play.keyCode == 37){
+					spaceship.Move(-20);
+				}
+				else if(play.keyCode == 32){
+					spaceship.Shoot();
+				}
 			}
-			else if(play.keyCode == 37){
-				spaceship.Move(-10);
-			}
-			else if(play.keyCode == 32){
-				spaceship.Shoot();
+			if(play.keyCode == 80){
+				pause = !pause;
 			}
 		});
 		game();
